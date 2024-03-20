67d72d7c9669839ae77fe9f80aa1a210403dcecd
hattieyang <62630777+hattieyang@users.noreply.github.com>
2024-03-19 21:06:31 -0400

    Update index.jsx

--- components/MainApp/index.jsx
+++ components/MainApp/index.jsx
@@ -7,24 +7,28 @@ import LoginWiondow from "components/LoginWiondow";
 
 
 export default function MainApp({ children, requireLogin = true }) {
-    const { productInfoData, loggedIn, loginWiondowData } = useContext(AppContext);
+    const { constants, loginInfo } = useContext(AppContext);
+    const { productInfoData, loginWiondowData } = constants;
+    //const { loggedIn } = loginInfo;
+    const loggedIn = true;
     const { header3Props } = productInfoData;
 
-    console.log("loginwindowdata", loginWiondowData)
+    console.log("mainapp loginInfo", loginInfo);
     return (
         <div style={{ display: "flex", flexFlow: "column nowrap", width: "100%" }}>
             <Header3 username141={header3Props.username141} logoContainerProps={header3Props.logoContainerProps} />
-            <div className="container" style={{ display: "flex", flexFlow: "row nowrap" }}>
-                <div className="col-lg-3" style={{ padding: "1.2em"}}>
-                    <SideNavMenu />
+            { (requireLogin && !loggedIn)
+                ? loginWiondowData && <FormProvider>{"NOT LOGGED IN"}<LoginWiondow {...loginWiondowData} /></FormProvider>
+                :
+                <div className="container" style={{ display: "flex", flexFlow: "row nowrap" }}>
+                    <div className="col-lg-3" style={{ padding: "1.2em", width: "fit-content" }}>
+                        <SideNavMenu />
+                    </div>
+                    <div className="col-lg-9" style={{ flexGrow: "1", display: "flex", flexFlow: "row nowrap", justifyContent: "center", padding: "1.2em" }}>
+                        {children}
+                    </div>
                 </div>
-                <div className="col-lg-9" style={{ flexGrow: "1", display: "flex", flexFlow: "row nowrap", padding: "1.2em" }}>
-                    {(requireLogin && !loggedIn)
-                        ?children
-                        : children
-                    }
-                </div>
-            </div>
+            }
             <div className="footer_-container" style={{ width: "100%", display: "flex", flexFlow: "column nowrap", justifyContent: "end" }}>
                 <Footer />
             </div>

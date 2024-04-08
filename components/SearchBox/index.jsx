8f0b51ee4a8f3b9ae67894a40752f746aa2c49c0
hattieyang <62630777+hattieyang@users.noreply.github.com>
2024-04-08 19:29:08 -0400

    Add files via upload

--- components/SearchBox/index.jsx
+++ components/SearchBox/index.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import "./SearchBox.sass";
 
-// Update the function to accept props
 function SearchBox(props) {
-  // Extract the placeholder prop from props, with a default value
   const {
-    placeholder = "输入产品名称、CAS进行搜索", // Default placeholder text
-    buttonImgSrc = "/img/component-1-7@2x.png" // Default image source for the button
+    placeholder = "输入产品名称、CAS进行搜索",
+    buttonImgSrc = "/img/component-1-7@2x.png",
+    onInputChange 
   } = props;
 
   return (
     <div className="search-box-new">
       <input
         type="text"
-        placeholder={placeholder} // Use the extracted placeholder prop
+        placeholder={placeholder}
+        onChange={onInputChange} 
         className="search-input"
       />
       <button>
         <img
-          src={buttonImgSrc} // Use the extracted buttonImgSrc prop for the image source
+          src={buttonImgSrc}
           alt="Search Icon"
           className="search-icon"
         />
